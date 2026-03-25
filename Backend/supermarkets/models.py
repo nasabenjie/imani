e518e4131d3feb61abdf27e4f3ce0dbd675ec10f
Nasasira Benjamin <bnasasira0@gmail.com>
2026-03-25 12:32:57 +0300

    upgraded supermarket model

--- Backend/supermarkets/models.py
+++ Backend/supermarkets/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
-
 
 class Supermarket(models.Model):
 
     name = models.CharField(max_length=255)
-
     location = models.CharField(max_length=255)
-
     region = models.CharField(max_length=255)
-
+    image = models.ImageField(
+        upload_to="supermarkets/",
+        blank=True,
+        null=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def image_url(self):
+        if self.image:
+            return self.image.url
+        return ""
